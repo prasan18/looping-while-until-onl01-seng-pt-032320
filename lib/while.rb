aab21461f73fc30a173c9bf795abf2670be17156
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-05 11:56:59 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -8,11 +8,6 @@ while levitation_force < 10 ## do this under certain circumstances
 	end
 	def using_until
 	 levitation_force = 6
-	until (levitation_force == 10) 
-  puts "Wingardium Leviosa"
-  
-  levitation_force += 1
-	end
-end
+	
 
 
